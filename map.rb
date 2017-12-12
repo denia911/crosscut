101822430e0c14840d9d20e592314ec49b65ae57
unknown <denisosa911@gmail.com>
2017-12-12 13:52:44 +0200

    Class Map in map.rb was modified. Duplicate methods were removed

--- map.rb
+++ map.rb
@@ -39,98 +39,88 @@ class Map
   end
 
   def find_path
-    count = 0
-    end_x = end_point_x
-    end_y = end_point_y.to_i
-    @map[start_point_y.to_i][start_point_x] = count
-    while @map[end_y][end_x] == 'x'
-      wave(count, 0)
-      count += 1
+    step = 0
+    @end_x = end_point_x
+    @end_y = end_point_y.to_i
+    start_x = start_point_x
+    start_y = start_point_y.to_i
+    integer_map
+    @map[start_y][start_x] = step
+    while @map[@end_y][@end_x] == -1
+      wave(step, 0)
+      step += 1
     end
   end
 
   def way
-    end_x = end_point_x
-    end_y = end_point_y.to_i
     find_path
-    puts "The shortest way = #{@map[end_y][end_x]} steps\n"
-    back_way_step(end_x, end_y)
+    puts "The shortest way = #{@map[@end_y][@end_x]} steps\n"
+    back_way_step(@end_x, @end_y)
   end
 
   private
 
-  def wave(count, y)
+  def integer_map
+    @map = @map.map do |longitude|
+    longitude.map do |element|
+      case element
+      when 's'
+        element = -2
+      else
+        element = -1
+      end
+      element
+    end
+    end
+  end
+
+  def wave(step, y)
     until y >= 10
       x = 0
       until x >= 10
-        neighbors(x, y, count)
+        neighbors(x, y, step)
         x += 1
       end
       y += 1
     end
   end
 
-  def neighbors(x, y, count)
-    @map[x][y - 1] = count + 1 if up?(x, y, count)
-    @map[x][y + 1] = count + 1 if down?(x, y, count)
-    @map[x - 1][y] = count + 1 if left?(x, y, count)
-    @map[x + 1][y] = count + 1 if right?(x, y, count)
+  def neighbors(x, y, step)
+    @map[x][y - 1] = step + 1 if direction?(x, y, step, y - 1)
+    @map[x][y + 1] = step + 1 if direction?(x, y, step, y + 1)
+    @map[x - 1][y] = step + 1 if direction?(x, y, step, x - 1)
+    @map[x + 1][y] = step + 1 if direction?(x, y, step, x + 1)
   end
 
-  def back_way_step(end_x, end_y)
+  def back_way_step(end_c_x, end_c_y)
+    end_x = end_c_x
+    end_y = end_c_y
     back_way = []
     until (@map[end_y][end_x]).zero?
       back_way << [end_y, end_x, @map[end_y][end_x]]
-      if back_way_up?(end_x, end_y)
+      if back?(end_y - 1) && @map[end_y][end_x] - @map[end_y - 1][end_x] == 1
         end_y -= 1
-      elsif back_way_left?(end_x, end_y)
+      elsif back?(end_x - 1) && @map[end_y][end_x] - @map[end_y][end_x - 1] == 1
         end_x -= 1
-      elsif back_way_right?(end_x, end_y)
+      elsif back?(end_x + 1) && @map[end_y][end_x] - @map[end_y][end_x + 1] == 1
         end_x += 1
-      elsif back_way_down?(end_x, end_y)
+      elsif back?(end_y + 1) && @map[end_y][end_x] - @map[end_y + 1][end_x] == 1
         end_y += 1
       end
     end
     back_way
   end
 
-  def down?(x, y, count)
-    y < 9 && @map[x][y + 1] != 's' && @map[x][y + 1].is_a?(String) &&
-      @map[x][y] == count
+  def direction?(x, y, step, direct)
+    @map[x][y] == step && (0..9).cover?(direct) &&
+      if direct == (y - 1) || direct == (y + 1)
+        @map[x][direct] == -1
+      elsif direct == (x + 1) || direct == (x - 1)
+        @map[direct][y] == -1
+      end
   end
 
-  def up?(x, y, count)
-    y.positive? && @map[x][y - 1] != 's' && @map[x][y - 1].is_a?(String) &&
-      @map[x][y] == count
-  end
-
-  def left?(x, y, count)
-    x.positive? && @map[x - 1][y] != 's' && @map[x - 1][y].is_a?(String) &&
-      @map[x][y] == count
-  end
-
-  def right?(x, y, count)
-    x < 9 && @map[x + 1][y] != 's' && @map[x + 1][y].is_a?(String) &&
-      @map[x][y] == count
-  end
-
-  def back_way_up?(end_x, end_y)
-    @map[end_y - 1][end_x].is_a?(Integer) &&
-      @map[end_y][end_x] - @map[end_y - 1][end_x] == 1
-  end
-
-  def back_way_left?(end_x, end_y)
-    @map[end_y][end_x - 1].is_a?(Integer) &&
-      @map[end_y][end_x] - @map[end_y][end_x - 1] == 1
-  end
-
-  def back_way_right?(end_x, end_y)
-    @map[end_y][end_x + 1].is_a?(Integer) &&
-      @map[end_y][end_x] - @map[end_y][end_x + 1] == 1
-  end
-
-  def back_way_down?(end_x, end_y)
-    @map[end_y + 1][end_x].is_a?(Integer) &&
-      @map[end_y][end_x] - @map[end_y + 1][end_x] == 1
+  def back?(direct)
+    (0..9).cover?(direct)
   end
 end
